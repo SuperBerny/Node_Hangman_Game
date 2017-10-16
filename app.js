6074c9541e887db3e78e1e5ecf1b6d642cd0c39a
Brian Patrick <batpat772@gmail.com>
2017-10-16 14:16:42 -0700

    hangman logic almost complete

--- app.js
+++ app.js
@@ -2,11 +2,69 @@
 //-----------------------------------------------
 //Arrays and variables for holding variables
 var wordOptions= ["san diego", "barcelona", "sydney", "chicago", "london"];
+var selectedWord ="";
+var lettersInWord = [];
+var numBlanks = 0;
+var blanksAndSuccesses = [];
+var wrongGuesses = [];
 
+//Game Counters
+var winCount = 0;
+var lossCount = 0;
+var guessesLeft = 9;
 
 
 
 //Functions
+//-----------------------------------------------
+function startGame(){
+    selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+    lettersInWord = selectedWord.split("");
+    numBlanks = lettersInWord.length;
+
+    //Reset
+    guessesLeft = 9;
+    wrongGuesses = [];
+    blanksAndSuccesses = [];
+
+    //Populate blanks and successes with correct number of blanks
+    for (var i = 0; i<numBlanks; i++){
+        blanksAndSuccesses.push("_");
+    }
+
+    //Change html to reflect round conditions
+
+    document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
+    document.getElementById("winCounter").innerHTML = winCount;
+    document.getElementById("lossCounter").innerHTML = lossCount;
+    console.log(selectedWord);
+    console.log(lettersInWord);
+    console.log(numBlanks);
+    console.log(blanksAndSuccesses);
+}
+
+function checkLetters(letter){
+    //check if the letter is anywhere in the word
+    alert(letter);
+    var letterInWord = false;
+    for(var i = 0; i<numBlanks; i++){
+        if (selectedWord[i] == letter) {
+            letterInWord = true;
+            alert("Letter Found");
+        }
+    }
+}
 
 //Main process
 // ----------------------------------------------
+//Initiates the game on pageload
+startGame();
+
+//Register key presses\
+document.onkeyup = function(event) {
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetters(letterGuessed);
+    //Testing/Debugging
+    console.log(letterGuessed);
+}
